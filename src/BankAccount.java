d4f219fea027127e06c9020bad0e12f6f9b75310
cptvictor <victorsyanwenliu19@gmail.com>
2021-07-28 17:46:07 -0400

    Flesh out BankAccount

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,53 @@
-public class BankAccount {
+import BankExceptions.NegativeAmountException;
+import BankExceptions.OverdraftException;
+
+public class BankAccount
+{
+    private UserAccount[] owners;
+    private double balance;
+    private String accountType;
+
+    public BankAccount(UserAccount[] owners, double balance, String accountType)
+    {
+        this.owners = owners;
+        this.balance = balance;
+        this.accountType = accountType;
+    }
+
+    public void deposit(double amount) throws NegativeAmountException
+    {
+        if(amount < 0)
+        {
+            throw new NegativeAmountException();
+        }
+        balance += amount;
+    }
+
+    public void withdraw(double amount) throws OverdraftException, NegativeAmountException
+    {
+        if(amount < 0)
+        {
+            throw new NegativeAmountException();
+        }
+        if(balance - amount < 0)
+        {
+            throw new OverdraftException();
+        }
+        balance -= amount;
+    }
+
+    public void addOwner(UserAccount owner)
+    {
+
+    }
+
+    public void removeOwner(UserAccount owner)
+    {
+
+    }
+
+    public String getAccountType()
+    {
+        return accountType;
+    }
 }
--- src/UserAccount.java
+++ src/UserAccount.java
@@ -1,2 +1,4 @@
-public class UserAccount {
+public class UserAccount
+{
+
 }
