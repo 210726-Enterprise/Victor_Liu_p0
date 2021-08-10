c80a2d23590fd77e7aa8b86e9842ff762d511752
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-10 10:28:54 -0400

    create setter for username

--- src/main/java/com/revature/model/UserAccount.java
+++ src/main/java/com/revature/model/UserAccount.java
@@ -40,6 +40,11 @@ public class UserAccount
         return false;
     }
 
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
     public String getUsername()
     {
         return username;
