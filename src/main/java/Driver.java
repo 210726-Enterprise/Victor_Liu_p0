0a0c73a84f98477e2928bd65675b43107e7fbc22
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-10 10:35:48 -0400

    temp driver

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,3 +1,4 @@
+import com.revature.bankexceptions.NegativeAmountException;
 import com.revature.collection.RevArrayList;
 import com.revature.model.BankAccount;
 import com.revature.model.UserAccount;
@@ -15,6 +16,59 @@ public class Driver
 {
     public static void main(String[] args)
     {
+
+        UserAccountDAO userDAO = new UserAccountDAOImplementation();
+        BankAccountDAO bankAccountDAO = new BankAccountDAOImplementation();
+
+        userDAO.insertUserAccount(new UserAccount("Test3", "Test3"));
+        UserAccount testUser = userDAO.getUserAccount("Test3");
+        bankAccountDAO.insertBankAccount(new BankAccount("Savings", 5000), testUser.getId());
+        bankAccountDAO.insertBankAccount(new BankAccount("Checking", 9000), testUser.getId());
+        RevArrayList<BankAccount> testBankList = bankAccountDAO.getAllBankAccounts(testUser.getId());
+
+        for(int i = 0; i < testBankList.size(); i++)
+        {
+            System.out.println(testBankList.get(i).getBalance());
+        }
+        System.out.println();
+
+        try {
+            testBankList.get(0).deposit(500);
+        } catch (NegativeAmountException e) {
+            e.printStackTrace();
+        }
+        bankAccountDAO.updateBankAccount(testBankList.get(0));
+        testUser.setUsername("Test4");
+        userDAO.updateUserAccount(testUser);
+
+        UserAccount testUser2 = userDAO.getUserAccount("Test4");
+        RevArrayList<BankAccount> testBankList2 = bankAccountDAO.getAllBankAccounts(testUser2.getId());
+        for(int i = 0; i < testBankList2.size(); i++)
+        {
+            System.out.println(testBankList.get(i).getBalance());
+        }
+        System.out.println();
+
+        bankAccountDAO.deleteBankAccount(testBankList2.get(0));
+
+//        UserAccount testUser2 = userDAO.getUserAccount("Test4");
+        RevArrayList<BankAccount> testBankList3 = bankAccountDAO.getAllBankAccounts(testUser2.getId());
+        for(int i = 0; i < testBankList3.size(); i++)
+        {
+            System.out.println(testBankList.get(i).getBalance());
+        }
+        userDAO.deleteUserAccount(testUser);
+        System.out.println();
+    }
+
+    public static void initialMenu()
+    {
+        System.out.println("Would you like to log in or create a new user account? (Select the number of your choice)");
+        System.out.println("1) Log in");
+        System.out.println("2) Create a new account");
+    }
+}
+
 //        Scanner scanner = new Scanner(System.in);
 //        String input = "";
 //
@@ -40,22 +94,3 @@ public class Driver
 //            default:
 //                System.out.println("Input invalid, please try again");
 //        }
-        UserAccountDAO userDAO = new UserAccountDAOImplementation();
-        UserAccount testUser = userDAO.getUserAccount("Test");
-
-        BankAccountDAO bankAccountDAO = new BankAccountDAOImplementation();
-        RevArrayList<BankAccount> testBankList = bankAccountDAO.getAllBankAccounts(testUser.getId());
-
-        System.out.println(testBankList.get(0).getBalance());
-
-
-        Connection connection = ConnectionFactory.getConnection();
-    }
-
-    public static void initialMenu()
-    {
-        System.out.println("Would you like to log in or create a new user account? (Select the number of your choice)");
-        System.out.println("1) Log in");
-        System.out.println("2) Create a new account");
-    }
-}
