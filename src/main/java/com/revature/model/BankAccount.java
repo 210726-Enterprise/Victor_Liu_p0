68aeb88c16e052a690c325a90a1aa06e51d7b005
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-10 21:48:43 -0400

    not sure where bankcontroller came from, refactored bankaccount constructors to throw exceptions

--- src/main/java/com/revature/model/BankAccount.java
+++ src/main/java/com/revature/model/BankAccount.java
@@ -11,62 +11,80 @@ public class BankAccount
     private double balance;
     private String accountType;
     private final int id;
+    private String name;
 
-    public BankAccount(int id, String accountType, double balance, RevaHashSet<UserAccount> owners)
+    public BankAccount(int id, String accountType, double balance, RevaHashSet<UserAccount> owners, String name) throws NegativeAmountException
     {
+        if(balance < 0)
+        {
+            throw new NegativeAmountException();
+        }
+        else
+        {
+            this.balance = balance;
+        }
         this.owners = owners;
         this.balance = balance;
         this.accountType = accountType;
         this.id = id;
+        this.name = name;
     }
 
-    public BankAccount(int id, String accountType, double balance, UserAccount owner)
+    public BankAccount(int id, String accountType, double balance, UserAccount owner, String name) throws NegativeAmountException
     {
+        if(balance < 0)
+        {
+            throw new NegativeAmountException();
+        }
+        else
+        {
+            this.balance = balance;
+        }
         this.owners = new RevaHashSet<>();
         this.owners.add(owner);
         this.balance = balance;
         this.accountType = accountType;
         this.id = id;
+        this.name = name;
     }
 
-    public BankAccount(int id, String accountType, double balance)
+    public BankAccount(int id, String accountType, double balance, String name) throws NegativeAmountException
     {
+        if(balance < 0)
+        {
+            throw new NegativeAmountException();
+        }
+        else
+        {
+            this.balance = balance;
+        }
         this.owners = new RevaHashSet<>();
         this.owners = null;
-        this.balance = balance;
         this.accountType = accountType;
         this.id = id;
+        this.name = name;
     }
 
-    public BankAccount(String accountType, double balance)
+    public BankAccount(String accountType, double balance, String name) throws NegativeAmountException
     {
+        if(balance < 0)
+        {
+            throw new NegativeAmountException();
+        }
+        else
+        {
+            this.balance = balance;
+        }
         this.owners = new RevaHashSet<>();
         this.owners = null;
-        this.balance = balance;
         this.accountType = accountType;
         this.id = 0;
+        this.name = name;
     }
 
-    public void deposit(double amount) throws NegativeAmountException
+    public void setBalance(double balance)
     {
-        if(amount < 0)
-        {
-            throw new NegativeAmountException();
-        }
-        balance += amount;
-    }
-
-    public void withdraw(double amount) throws OverdraftException, NegativeAmountException
-    {
-        if(amount < 0)
-        {
-            throw new NegativeAmountException();
-        }
-        if(balance - amount < 0)
-        {
-            throw new OverdraftException();
-        }
-        balance -= amount;
+        this.balance = balance;
     }
 
     public void addOwner(UserAccount owner)
@@ -97,5 +115,13 @@ public class BankAccount
     public int getId()
     {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
