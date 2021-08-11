8bae4c15815b6ba230280fc5914cdd4bc9223946
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-11 01:11:23 -0400

    utility class

---
+++ src/main/java/com/revature/util/BankUtilities.java
@@ -0,0 +1,25 @@
+package com.revature.util;
+
+public class BankUtilities
+{
+    public static String generateAccountNumber()
+    {
+        return "" + Math.random()*1000000000;
+    }
+
+    public static String generateRoutingNumber()
+    {
+        return "" + Math.random()*10000000;
+    }
+
+    public static String formatIntoCurrency(double number)
+    {
+        String currency =  "$" + number;
+        String decimalSplit = currency.substring(currency.indexOf(".")+1);
+        if(decimalSplit.length() < 2)
+        {
+            currency += "0";
+        }
+        return currency;
+    }
+}
