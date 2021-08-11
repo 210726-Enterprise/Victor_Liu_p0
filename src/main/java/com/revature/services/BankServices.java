48b81860a64f097d3fc509c01a684199ad493c69
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-10 23:02:35 -0400

    interface and implementation for accessing and modifying bank accounts

---
+++ src/main/java/com/revature/services/BankServices.java
@@ -0,0 +1,22 @@
+package com.revature.services;
+
+import com.revature.bankexceptions.NegativeAmountException;
+import com.revature.bankexceptions.OverdraftException;
+import com.revature.model.BankAccount;
+import com.revature.model.UserAccount;
+import com.revature.repo.UserAccountDAO;
+
+public interface BankServices
+{
+    void makeDeposit(BankAccount account, double amount) throws NegativeAmountException;
+
+    void makeWithdrawal(BankAccount account, double amount) throws NegativeAmountException, OverdraftException;
+
+    void transfer(BankAccount accountToWithdraw, BankAccount accountToDeposit, double amount) throws NegativeAmountException, OverdraftException;
+
+    double viewBalance(BankAccount account);
+
+    void addOwner(BankAccount account, UserAccount newOwner);
+
+    void viewTransactionHistory(BankAccount account);
+}
---
+++ src/main/java/com/revature/services/BankServicesImplementation.java
@@ -0,0 +1,72 @@
+package com.revature.services;
+
+import com.revature.bankexceptions.NegativeAmountException;
+import com.revature.bankexceptions.OverdraftException;
+import com.revature.model.BankAccount;
+import com.revature.model.UserAccount;
+import com.revature.repo.BankAccountDAO;
+import com.revature.repo.BankAccountDAOImplementation;
+
+public class BankServicesImplementation implements BankServices
+{
+    private static BankAccountDAO bankAccountDAO = new BankAccountDAOImplementation();
+
+    @Override
+    public void makeDeposit(BankAccount account, double amount) throws NegativeAmountException
+    {
+        if(amount < 0)
+        {
+            throw new NegativeAmountException();
+        }
+        account.setBalance(account.getBalance()+amount);
+        bankAccountDAO.updateBankAccount(account);
+    }
+
+    @Override
+    public void makeWithdrawal(BankAccount account, double amount) throws NegativeAmountException, OverdraftException
+    {
+        if(amount < 0)
+        {
+            throw new NegativeAmountException();
+        }
+        if(account.getBalance() - amount < 0)
+        {
+            throw new OverdraftException();
+        }
+        account.setBalance(account.getBalance()-amount);
+        bankAccountDAO.updateBankAccount(account);
+    }
+
+    @Override
+    public void transfer(BankAccount accountToWithdraw, BankAccount accountToDeposit, double amount) throws NegativeAmountException, OverdraftException
+    {
+        if(amount < 0)
+    {
+        throw new NegativeAmountException();
+    }
+        if(accountToWithdraw.getBalance() - amount < 0)
+        {
+            throw new OverdraftException();
+        }
+        accountToWithdraw.setBalance(accountToWithdraw.getBalance()-amount);
+        accountToDeposit.setBalance(accountToDeposit.getBalance()+amount);
+        bankAccountDAO.updateBankAccount(accountToWithdraw);
+        bankAccountDAO.updateBankAccount(accountToDeposit);
+    }
+
+    @Override
+    public double viewBalance(BankAccount account)
+    {
+
+    }
+
+    @Override
+    public void addOwner(BankAccount account, UserAccount newOwner) {
+
+    }
+
+    @Override
+    public void viewTransactionHistory(BankAccount account) {
+
+    }
+}
